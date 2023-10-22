2a7cdf0749b838ab5f476112db2d20495f846e7c
reistence <gvlamberti@gmail.com>
2023-10-22 11:30:15 +0200

    theatre setting up

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,54 @@
-import { Canvas } from "@react-three/fiber";
+import { Canvas, useFrame } from "@react-three/fiber";
 import "./App.css";
-import { OrbitControls } from "@react-three/drei";
+import { ScrollControls, useScroll } from "@react-three/drei";
+import { getProject, val } from "@theatre/core";
+import {
+  SheetProvider,
+  PerspectiveCamera,
+  useCurrentSheet,
+} from "@theatre/r3f";
 import { Spaceboi } from "./components/Spaceboi";
 
 function App() {
+  const sheet = getProject("Fly Through").sheet("Scene");
+
   return (
     <>
-      <Canvas>
-        <OrbitControls
-          enableDamping
-          minDistance={5} // Set your desired minimum zoom level
-          maxDistance={20}
-          target={[0, 0, 0]}
-        />
-
-        <Spaceboi />
+      <Canvas gl={{ preserveDrawingBuffer: true }}>
+        <ScrollControls pages={5}>
+          <SheetProvider sheet={sheet}>
+            <Scene />
+          </SheetProvider>
+        </ScrollControls>
       </Canvas>
     </>
   );
 }
 
 export default App;
+
+function Scene() {
+  const sheet = useCurrentSheet();
+  const scroll = useScroll();
+
+  useFrame(() => {
+    const sequenceLength = val(sheet.sequence.pointer.length);
+
+    sheet.sequence.position = scroll.offset * sequenceLength;
+  });
+
+  return (
+    <>
+      {/* <color attach='background' args={['black']}/> */}
+      <PerspectiveCamera
+        theatreKey="Camera"
+        makeDefault
+        position={[0, 4, 15]}
+        fov={100}
+        near={0.5}
+        far={100}
+      />
+      <Spaceboi />
+    </>
+  );
+}
